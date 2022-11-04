b9011536d8cf6980ff59e41610fcf11ffbae7022
jimenanietocerezo <89277578+jimenanietocerezo@users.noreply.github.com>
2022-11-04 11:50:49 -0300

    NODO2"modifico rutas y modelos de personas"

--- src/models/person.model.js
+++ src/models/person.model.js
@@ -5,6 +5,9 @@ const PersonSchema = new Schema({
   name: { type: String, required: true },
   surname: { type: String, required: true },
   email: { type: String, requiered: true },
+  dni: { type: String, requiered: true },
+  phone: { type: String, requiered: true },
+  dateBirth:{ type: String, requiered: true },
   date: { type: Date, default: Date.now },
 });
 
